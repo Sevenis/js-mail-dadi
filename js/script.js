6e200afc79a1daba2cfd862d88ad6e6713f393ae
Pietro Sette <pietrosette@gmail.com>
2020-07-08 17:08:09 +0200

    Check email done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
+var listaEmails = ['bill@microsoft.com', 'sjobs@apple.com', 'mr_z@facebook.com']
+var email = prompt('Inserire la propria email');
+var checkMail = 'Email non trovata!';
 
+for(i = 0; i < listaEmails.length; i++){
+    if(email == listaEmails[i]) {
+        checkMail = 'Email trovata!';
+    }
+}
+
+document.getElementById('check-email').innerHTML = checkMail;
